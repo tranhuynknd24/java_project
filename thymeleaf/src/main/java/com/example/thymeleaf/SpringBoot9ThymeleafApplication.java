93d332859f6e050e76e7cc4d6aadaff1adbdbc29
Tran Huy <huytran031297@gmail.com>
2023-11-04 18:40:08 +0700

    [JAVA-0003] Init project: Create simple web with Expression in Thymeleaf

---
+++ thymeleaf/src/main/java/com/example/thymeleaf/SpringBoot9ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot9ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot9ThymeleafApplication.class, args);
+	}
+
+}
---
+++ thymeleaf/src/test/java/com/example/thymeleaf/SpringBoot9ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot9ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
