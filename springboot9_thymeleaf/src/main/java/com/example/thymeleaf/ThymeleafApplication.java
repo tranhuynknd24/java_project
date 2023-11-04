5d21a1049933b67d069047773be29b6ed26023df
Tran Huy <huytran031297@gmail.com>
2023-11-04 18:58:04 +0700

    [JAVA-0003] Rename project

--- springboot9_thymeleaf/src/main/java/com/example/thymeleaf/ThymeleafApplication.java
+++ springboot9_thymeleaf/src/main/java/com/example/thymeleaf/ThymeleafApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot9ThymeleafApplication {
+public class ThymeleafApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoot9ThymeleafApplication.class, args);
+		SpringApplication.run(ThymeleafApplication.class, args);
 	}
 
 }
--- springboot9_thymeleaf/src/test/java/com/example/springboot9_thymeleaf/Springboot9ThymeleafApplicationTests.java
+++ springboot9_thymeleaf/src/test/java/com/example/springboot9_thymeleaf/Springboot9ThymeleafApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.thymeleaf;
+package com.example.springboot9_thymeleaf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBoot9ThymeleafApplicationTests {
+class Springboot9ThymeleafApplicationTests {
 
 	@Test
 	void contextLoads() {
