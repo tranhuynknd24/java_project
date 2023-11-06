ab3d9111faf305a13a41ffe685bb5ef9cdc040d8
Tran Huy <huytran031297@gmail.com>
2023-11-06 23:30:20 +0700

    [JAVA-0006] Init project: JPA + @Query

---
+++ springboot12_jpaquery/src/main/java/com/example/jpaquery/JpaqueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaqueryApplication.class, args);
+	}
+
+}
---
+++ springboot12_jpaquery/src/test/java/com/example/jpaquery/JpaqueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaqueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
