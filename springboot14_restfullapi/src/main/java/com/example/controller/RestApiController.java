30f53cfa2c5ffd600e90ee54a5e785020f334930
Tran Huy <huytran031297@gmail.com>
2023-11-08 23:37:09 +0700

    [JAVA-0007] Restruct package

--- springboot14_restfullapi/src/main/java/com/example/controller/RestApiController.java
+++ springboot14_restfullapi/src/main/java/com/example/controller/RestApiController.java
@@ -1,4 +1,4 @@
-package com.example.restfullapi;
+package com.example.controller;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Todo;
 
 import jakarta.annotation.PostConstruct;
 
--- springboot14_restfullapi/src/main/java/com/example/model/Todo.java
+++ springboot14_restfullapi/src/main/java/com/example/model/Todo.java
@@ -1,4 +1,4 @@
-package com.example.restfullapi;
+package com.example.model;
 
 import lombok.Data;
 
