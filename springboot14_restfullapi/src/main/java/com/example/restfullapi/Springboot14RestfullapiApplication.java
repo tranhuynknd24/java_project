8f63932f3c3f130041f63443def73ec3f1918961
Tran Huy <huytran031297@gmail.com>
2023-11-08 22:43:30 +0700

    [JAVA-0007] Init project: create restful api

---
+++ springboot14_restfullapi/src/main/java/com/example/restfullapi/Springboot14RestfullapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfullapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot14RestfullapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot14RestfullapiApplication.class, args);
+	}
+
+}
