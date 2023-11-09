b08ad6e220cfa71fdeccf6fde53e9f999e01d3a7
Tran Huy <huytran031297@gmail.com>
2023-11-09 23:30:17 +0700

    [JAVA-0008] Init project: create work management website

---
+++ springboot13_workmanagement/src/main/java/com/example/workmanagement/Springboot13WorkmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.workmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot13WorkmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot13WorkmanagementApplication.class, args);
+	}
+
+}
---
+++ springboot13_workmanagement/src/test/java/com/example/workmanagement/Springboot13WorkmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot13WorkmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
