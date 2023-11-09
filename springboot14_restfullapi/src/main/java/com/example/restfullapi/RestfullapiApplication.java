87d563ac89fc71944d185f9096cdf8c0e8036f9a
Tran Huy <huytran031297@gmail.com>
2023-11-09 21:56:45 +0700

    [JAVA-0007] Add @ComponentScan to get Bean

--- springboot14_restfullapi/src/main/java/com/example/restfullapi/RestfullapiApplication.java
+++ springboot14_restfullapi/src/main/java/com/example/restfullapi/RestfullapiApplication.java
@@ -2,7 +2,9 @@ package com.example.restfullapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.example.controller")
 @SpringBootApplication
 public class RestfullapiApplication {
 
